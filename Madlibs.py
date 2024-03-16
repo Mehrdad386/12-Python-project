9602ad564fe0982311d473ce47b8bcc841cdb9cc
Mehrdad <mehrdadmrs10@gmail.com>
2024-03-16 17:29:31 +0330

    Madlibs.py added

---
+++ Madlibs.py
@@ -0,0 +1,11 @@
+adj = input("Adejctive: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ") 
+famousePerson = input("Famous person: ")
+
+
+Madlibs = f"computer programming is so {adj}! it makes me so excited all the time because \
+i love to {verb1}. stay hydrated  and {verb2} like you are {famousePerson}!"
+
+
+print(Madlibs) 
